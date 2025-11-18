9f7e98adb8710c5882aa86f52fd5296c95eb9edb
Yu Okumura <yu@furo.org>
2025-11-18 19:28:09 +0900

    Update setup_raspi.sh

--- setup/setup_raspi.sh
+++ setup/setup_raspi.sh
@@ -1,4 +1,5 @@
 #Enable Japanease Localization
+sudo raspi-config nonint do_vnc 0
 sudo raspi-config nonint do_change_locale ja_JP.UTF-8
 sudo raspi-config nonint do_change_timezone Asia/Tokyo
 #Enable i2c
